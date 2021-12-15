ad25d88459dd8297fd50a59ac22ea5062730c5e2
rock12231 <37548802+rock12231@users.noreply.github.com>
2021-12-16 00:20:33 +0530

    on off push button

---
+++ push_btn_on_off/push_btn_on_off.ino
@@ -0,0 +1,27 @@
+#define LED_PIN D2
+#define BUTTON_PIN D1
+
+byte lastButtonState = LOW;
+byte ledState = LOW;
+
+unsigned long debounceDuration = 50; // millis
+unsigned long lastTimeButtonStateChanged = 0;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+void loop() {
+  if (millis() - lastTimeButtonStateChanged > debounceDuration) {
+    byte buttonState = digitalRead(BUTTON_PIN);
+    if (buttonState != lastButtonState) {
+      lastTimeButtonStateChanged = millis();
+      lastButtonState = buttonState;
+      if (buttonState == LOW) {
+        ledState = (ledState == HIGH) ? LOW: HIGH;
+        digitalWrite(LED_PIN, ledState);
+      }
+    }
+  }
+}
