e30a7fbcfe38efb5d369417202b5b44882b3acd2
rock12231 <37548802+rock12231@users.noreply.github.com>
2021-12-17 01:18:52 +0530

    New code tone

---
+++ TONE/TONE.ino
@@ -0,0 +1,79 @@
+//A-ha!
+//by GeneralSpud
+
+// For this to work, we need the pitches library
+
+#include "pitches.h"
+
+// Two things need to be created: the array for the notes of the melody (in order)
+// and the duration of each (think of it like sheet music in two parts)
+
+// BOTH ARRAYS MUST BE THE SAME SIZE!
+
+// The melody array 
+int melody[] = {
+ NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+  NOTE_CS4, NOTE_DS4,
+  NOTE_DS4, NOTE_GS3,
+  NOTE_G3, NOTE_CS4,
+  NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+  NOTE_GS4, NOTE_DS4, NOTE_B3,
+  NOTE_AS3, NOTE_A3, NOTE_GS3,
+  0, 0, 0
+};
+
+// The note duration, 8 = 8th note, 4 = quarter note, etc.
+int durations[] = {
+ 12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  6, 18, 18, 18,
+  6, 6,
+  6, 6,
+  6, 6,
+  18, 18, 18, 18, 18, 18,
+  10, 10, 10,
+  10, 10, 10,
+  3, 3, 3
+};
+// determine the length of the arrays to use in the loop iteration
+int songLength = sizeof(melody)/sizeof(melody[0]);
+void setup() {
+ //We don't need anything here
+}
+
+void loop() {
+  // Iterate through both arrays
+  // Notice how the iteration variable thisNote is created in the parenthesis
+  // The for loop stops when it is equal to the size of the melody array
+  for (int thisNote = 0; thisNote < songLength; thisNote++){
+    // determine the duration of the notes that the computer understands
+    // divide 1000 by the value, so the first note lasts for 1000/8 milliseconds
+    int duration = 1000/ durations[thisNote];
+    tone(D2, melody[thisNote], duration);
+    // pause between notes
+    int pause = duration * 1.3;
+    delay(pause);
+    // stop the tone
+    noTone(D2);
+  }
+}
